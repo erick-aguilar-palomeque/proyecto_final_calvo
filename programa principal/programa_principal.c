2649d2bb5ecd1ab16a1852497b8c0ab7f6f80b84
spokesblind <aguilar505088@gmail.com>
2020-04-06 15:46:01 -0500

    Terminé el menu, solo falta reportes, hicé un diagrama de la estructura del programa también

--- programa principal/programa_principal.c
+++ programa principal/programa_principal.c
@@ -1,18 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <postgresql/libpq-fe.h>
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
 void hacer_select();
 int menu_principal();
+
 int menu_pacientes();
+void alta_pacientes();
+void actualizar_pacientes();
+void buscar_pacientes();
+
 int menu_laboratoristas();
+void alta_laboratoristas();
+void actualizar_laboratoristas();
+void buscar_laboratoristas();
+void despedir_laboratoristas();
+
 int menu_analisis();
-int menu_materiales_reactivos();
+void solicitar_analisis();
+void realizar_analisis();
+void entregar_analisis();
+void buscar_analisis();
+void consultas_analisis();
+void agregar_nuevo_analisis();
+
+int menu_materiales();
+void alta_materiales();
+void baja_materiales();
+
+int menu_reactivos();
+void alta_reactivos();
+void baja_reactivos();
+
 int menu_reportes();
 PGconn *conn;
 PGresult *res;
 PGresult *resultado;
 int main(int argc, char *argv[]){
-    int opc, opc_paciente, opc_laboratorista, opc_analisis, opc_materiales, opc_reportes;
+    int opc, opc_paciente, opc_laboratorista, opc_analisis, opc_materiales,opc_reactivos, opc_reportes;
     conn = PQsetdbLogin("localhost","5432",NULL,NULL,"lac","usuario1","usuario1");
     if(PQstatus(conn) != CONNECTION_BAD){
         printf("La conexion a la base de datos ha sido correcta\n");
@@ -21,67 +48,246 @@ int main(int argc, char *argv[]){
         opc=menu_principal();
         switch (opc)
         {
-        case 1: 
+        case 1: system("clear");
             do{
             opc_paciente=menu_pacientes(); 
                 switch (opc_paciente){
-                    case 1: break;
-                    case 2: break;
-                    case 3: break;
-                    case 4: break;
-                    default: printf("Opcion no valida, intente de nuevo\n"); break;
+                    case 1: system("clear"); alta_pacientes(); break;
+                    case 2: system("clear"); actualizar_pacientes(); break;
+                    case 3: system("clear"); buscar_pacientes(); break;
+                    case 4: system("clear"); break;
+                    default: system("clear");
+                    printf(ANSI_COLOR_RED "Opcion no valida, intente de nuevo\n\n" ANSI_COLOR_RESET ); break;
                 }
-            }while(opc != 4);
+            }while(opc_paciente != 4);
             break;
 
-        case 2: menu_laboratoristas(); break;
+        case 2: system("clear");
+            do{
+            opc_laboratorista=menu_laboratoristas(); 
+                switch (opc_laboratorista){
+                    case 1: system("clear"); alta_laboratoristas(); break;
+                    case 2: system("clear"); actualizar_laboratoristas(); break;
+                    case 3: system("clear"); buscar_laboratoristas(); break;
+                    case 4: system("clear"); despedir_laboratoristas(); break;
+                    case 5: system("clear"); break;
+                    default: system("clear");
+                    printf(ANSI_COLOR_RED "Opcion no valida, intente de nuevo\n\n" ANSI_COLOR_RESET ); break;
+                }
+            }while(opc_laboratorista != 5);break;
 
-        case 3: menu_analisis(); break;
+        case 3: system("clear"); 
+            do{
+            opc_analisis=menu_analisis(); 
+                switch (opc_analisis){
+                    case 1: system("clear"); solicitar_analisis(); break;
+                    case 2: system("clear"); realizar_analisis(); break;
+                    case 3: system("clear"); entregar_analisis(); break;
+                    case 4: system("clear"); buscar_analisis(); break;
+                    case 5: system("clear"); consultas_analisis(); break;
+                    case 6: system("clear"); agregar_nuevo_analisis(); break;
+                    case 7: system("clear"); break;
+                    default: system("clear");
+                    printf(ANSI_COLOR_RED "Opcion no valida, intente de nuevo\n\n" ANSI_COLOR_RESET ); break;
+                }
+            }while(opc_analisis != 7);break;
 
-        case 4: menu_materiales_reactivos(); break;
+        case 4: system("clear"); 
+        do{
+            opc_materiales=menu_materiales(); 
+                switch (opc_materiales){
+                    case 1: system("clear"); alta_materiales(); break;
+                    case 2: system("clear"); baja_materiales(); break;
+                    case 3: system("clear"); break;
+                    default: system("clear");
+                    printf(ANSI_COLOR_RED "Opcion no valida, intente de nuevo\n\n" ANSI_COLOR_RESET ); break;
+                }
+            }while(opc_materiales != 3);break;
 
-        case 5: menu_reportes(); break;
+        case 5: system("clear");
+        do{
+            opc_reactivos=menu_reactivos(); 
+                switch (opc_reactivos){
+                    case 1: system("clear"); alta_reactivos(); break;
+                    case 2: system("clear"); baja_reactivos(); break;
+                    case 3: system("clear"); break;
+                    default: system("clear");
+                    printf(ANSI_COLOR_RED "Opcion no valida, intente de nuevo\n\n" ANSI_COLOR_RESET ); break;
+                }
+            }while(opc_reactivos != 3);break;
 
-        case 6: break;
+        case 6: system("clear"); 
+        menu_reportes(); break;
 
-        default: printf("Opcion no valida, intente de nuevo\n"); break;
+        case 7: system("clear"); break;
+
+        default: system("clear"); 
+        printf(ANSI_COLOR_RED "Opcion no valida, intente de nuevo\n\n" ANSI_COLOR_RESET ); break;
         }
 
-    }while (opc != 6);
+    }while (opc != 7);
 
     PQfinish(conn);
     return 0;
 }
 int menu_principal(){
     int opc;
-    printf("\n\n|-------------------MENU PRINCIPAL-------------------|\n");
+    printf("|-------------------MENU PRINCIPAL-------------------|");
     printf("\n[1] PACIENTES");
     printf("\n[2] LABORATORISTAS");
     printf("\n[3] ANALISIS");
-    printf("\n[4] MATERIALES/REACTIVOS");
-    printf("\n[5] REPORTES");
-    printf("\n[6] SALIR");
-    printf("\n------------------------------------------------------\n");
+    printf("\n[4] MATERIALES");
+    printf("\n[5] REACTIVOS");
+    printf("\n[6] REPORTES");
+    printf("\n[7] SALIR");
+    printf("\n----------------------------------------------------\n");
     printf("Ingrese la opcion deseada : ");
     scanf("%d", &opc);
     return opc;    
 }
-int menu_pacientes(){system("clear");
-    printf("Pacientes\n");
-    return 0;
+
+
+int menu_pacientes(){
+ int opc;
+    printf("|------------------MENU PACIENTES------------------|");
+    printf("\n[1] ALTA");
+    printf("\n[2] ACTUALIZAR");
+    printf("\n[3] BUSCAR");
+    printf("\n[4] VOLVER AL MENU PRINCIPAL");
+    printf("\n---------------------------------------------------\n");
+    printf("Ingrese la opcion deseada : ");
+    scanf("%d", &opc);
+    return opc;    
 }
-int menu_laboratoristas(){system("clear");
-    printf("Laboratoristas\n");
-    return 0;
+void alta_pacientes(){ 
+    printf("|------------------ALTA PACIENTES------------------|\n");
+    printf("---------------------------------------------------\n\n\n");
 }
-int menu_analisis(){system("clear");
-    printf("Analisis\n");
-    return 0;
+void actualizar_pacientes(){
+    printf("|---------------ACTUALIZAR PACIENTES---------------|\n");
+    printf("---------------------------------------------------\n\n\n");
 }
-int menu_materiales_reactivos(){system("clear");
-    printf("Materiales/Reactivos\n");
-    return 0;
+void buscar_pacientes(){
+    printf("|-----------------BUSCAR PACIENTES-----------------|\n");
+    printf("---------------------------------------------------\n\n\n");
 }
+
+
+int menu_laboratoristas(){
+    int opc;
+    printf("|--------------MENU  LABORATORISTAS----------------|");
+    printf("\n[1] ALTA");
+    printf("\n[2] ACTUALIZAR");
+    printf("\n[3] BUSCAR");
+    printf("\n[4] DESPEDIR");
+    printf("\n[5] VOLVER AL MENU PRINCIPAL");
+    printf("\n---------------------------------------------------\n");
+    printf("Ingrese la opcion deseada : ");
+    scanf("%d", &opc);
+    return opc;    
+}
+void alta_laboratoristas(){
+    printf("|----------------ALTA LABORATORISTA----------------|\n");
+    printf("---------------------------------------------------\n\n\n");
+}
+void actualizar_laboratoristas(){
+    printf("|-------------ACTUALIZAR LABORATORISTA-------------|\n");
+    printf("---------------------------------------------------\n\n\n");
+}
+void buscar_laboratoristas(){
+    printf("|---------------BUSCAR LABORATORISTA---------------|\n");
+    printf("---------------------------------------------------\n\n\n");
+}
+void despedir_laboratoristas(){
+    printf("|--------------DESPEDIR LABORATORISTA--------------|\n");
+    printf("---------------------------------------------------\n\n\n");
+}
+
+
+int menu_analisis(){
+    int opc;
+    printf("|-----------------MENU  ANALISIS-------------------|");
+    printf("\n[1] SOLICITAR ANALISIS");
+    printf("\n[2] REALIZAR ANALISIS");
+    printf("\n[3] ENTREGAR ANALISIS");
+    printf("\n[4] BUSCAR ANALISIS");
+    printf("\n[5] CONSULTAS ANALISIS");
+    printf("\n[6] AGREGAR UN NUEVO ANALISIS");
+    printf("\n[7] VOLVER AL MENU PRINCIPAL");
+    printf("\n---------------------------------------------------\n");
+    printf("Ingrese la opcion deseada : ");
+    scanf("%d", &opc);
+    return opc; 
+}
+void solicitar_analisis(){
+    printf("|----------------SOLICITAR ANALISIS----------------|\n");
+    printf("---------------------------------------------------\n\n\n");
+}
+void realizar_analisis(){
+    printf("|----------------REALIZAR  ANALISIS----------------|\n");
+    printf("---------------------------------------------------\n\n\n");
+}
+void entregar_analisis(){
+    printf("|----------------ENTREGAR  ANALISIS----------------|\n");
+    printf("---------------------------------------------------\n\n\n");
+}
+void buscar_analisis(){
+    printf("|-----------------BUSCAR  ANALISIS-----------------|\n");
+    printf("---------------------------------------------------\n\n\n");
+}
+void consultas_analisis(){
+    printf("|----------------CONSULTAS ANALISIS----------------|\n");
+    printf("---------------------------------------------------\n\n\n");
+}
+void agregar_nuevo_analisis(){
+    printf("|--------------AGREGAR NUEVO ANALISIS--------------|\n");
+    printf("---------------------------------------------------\n\n\n");
+}
+
+
+
+int menu_materiales(){
+    int opc;
+    printf("|----------------MENU  MATERIALES------------------|");
+    printf("\n[1] ALTA");
+    printf("\n[2] BAJA");
+    printf("\n[3] VOLVER AL MENU PRINCIPAL");
+    printf("\n---------------------------------------------------\n");
+    printf("Ingrese la opcion deseada : ");
+    scanf("%d", &opc);
+    return opc; 
+}
+void alta_materiales(){
+    printf("|-----------------ALTA  MATERIALES-----------------|\n");
+    printf("---------------------------------------------------\n\n\n");
+}
+void baja_materiales(){
+    printf("|-----------------BAJA  MATERIALES-----------------|\n");
+    printf("---------------------------------------------------\n\n\n");
+}
+
+
+int menu_reactivos(){
+    int opc;
+    printf("|-----------------MENU REACTIVOS-------------------|");
+    printf("\n[1] ALTA");
+    printf("\n[2] BAJA");
+    printf("\n[3] VOLVER AL MENU PRINCIPAL");
+    printf("\n---------------------------------------------------\n");
+    printf("Ingrese la opcion deseada : ");
+    scanf("%d", &opc);
+    return opc; 
+}
+void alta_reactivos(){
+    printf("|------------------ALTA REACTIVOS------------------|\n");
+    printf("---------------------------------------------------\n\n\n");
+}
+void baja_reactivos(){
+    printf("|------------------BAJA REACTIVOS------------------|\n");
+    printf("---------------------------------------------------\n\n\n");    
+}
+
+
 int menu_reportes(){system("clear");
     printf("reportes\n");
     return 0;
